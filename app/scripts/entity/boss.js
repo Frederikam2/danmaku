25984f047e90978b4fba2768da40e52c773c5325
Frederikam <frogkr@gmail.com>
2018-01-03 01:58:07 +0100

    Make the boss move around

--- app/scripts/entity/boss.js
+++ app/scripts/entity/boss.js
@@ -8,8 +8,30 @@ module.newBoss = function (stage) {
   module.entity = new createjs.Shape();
   module.entity.graphics.beginFill('cyan').drawRect(-20, -20, 40, 40);
   module.entity.x = document.getElementById('game').width/2;
-  module.entity.y = 100;
+  module.entity.y = 200;
   stage.addChild(module.entity);
+
+  let canvas = document.getElementById("game");
+
+
+
+  const points = [
+    {x: canvas.width - 50, y: 50},
+    {x: 50, y: 50},
+    {x: canvas.width / 2, y: 200},
+  ];
+
+  createjs.Tween.get(module.entity, {loop: true})
+    .wait(5000)
+    .to(points[0], 1000, createjs.Ease.sineInOut(2))
+    .wait(5000)
+    .to(points[1], 1000, createjs.Ease.sineInOut(2))
+    .wait(5000)
+    .to(points[2], 1000, createjs.Ease.sineInOut(2));
+
+  function handleComplete() {
+    //Tween complete
+  }
 };
 
 module.takeHit = function() {
